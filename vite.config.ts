3e7c888655516c460ec033b63667324cd4fcbf22
luandangit@gmail.com <luandangit@gmail.com>
2023-05-30 21:36:37 +0700

    Task_#0 Reactjs base add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/the-ocean-villas",
   plugins: [react()],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 })
